ebf69d0ab42bbd09f885ef0abf4a2eea022c173f
Paul Sexton <psexton@gnresound.com>
2015-01-21 15:24:32 -0600

    feat(java): ant jar task outputs `missing-http.jar`
    Running from the command line will print the version of HttpComponents used to build the jar.

---
+++ java/src/net/psexton/missinghttp/Main.java
@@ -0,0 +1,16 @@
+/*
+ * Main.java, part of the missing-http project
+ * Created on Jan 21, 2015, 3:07:23 PM
+ */
+
+package net.psexton.missinghttp;
+
+/**
+ * Mainly here so that the jar has a Main Class.
+ * @author PSexton
+ */
+class Main {
+    public static void main(String[] args) {
+        System.out.println("missing-http, build with HttpComponents Client 4.3.3 GA");
+    }
+}
