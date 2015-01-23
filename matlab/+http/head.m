d3d18cbc5a70fa0fb6d53bd32481d5ce081ed782
Paul Sexton <psexton@gnresound.com>
2015-01-23 17:48:41 +0000

    refactor(matlab): replace jsonHead with head
    BREAKING CHANGE:
    Function `http.jsonHead` is removed. Use function `http.head` instead,
    and add a header of "Accept: application/json".

--- matlab/+http/head.m
+++ matlab/+http/head.m
@@ -1,16 +1,16 @@
-function statusCode = jsonHead( url, varargin )
-%JSONHEAD Makes a HEAD request, asking for a JSON response
+function statusCode = head( url, varargin )
+%HEAD Makes a HEAD request, asking for a JSON response
 %   URL resource to make the request to
 %   VARARGIN extra headers to add to the request
 %   STATUSCODE Integer response code
 %   
 %   HEAD requests behave exactly like GET requests, except no response body
 %   is returned. Occasionally useful for prepopulating client-side host / 
-%   auth caches.
+%   auth caches. Also useful for checking auth before making a large POST 
+%   or PUT request to a server that doesn't support 100 Continue.
 
 % Build the request
 request = net.psexton.ext.org.apache.http.client.methods.HttpHead(url);
-request.setHeader('Accept', 'application/json');
 if(nargin > 1)
     http.private.addExtraHeaders(request, varargin);
 end
