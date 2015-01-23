73495590649253229e4c5973177023fed7e28570
Paul Sexton <psexton@gnresound.com>
2015-01-23 18:21:57 +0000

    refactor(java): move version msg from main to Main ctr

--- java/src/net/psexton/missinghttp/Main.java
+++ java/src/net/psexton/missinghttp/Main.java
@@ -7,10 +7,15 @@ package net.psexton.missinghttp;
 
 /**
  * Mainly here so that the jar has a Main Class.
+ * Also prints out to stdout the version info of HttpComponents we built with.
  * @author PSexton
  */
 class Main {
     public static void main(String[] args) {
+        Main main = new Main();
+    }
+    
+    public Main() {
         System.out.println("missing-http, build with HttpComponents Client 4.3.3 GA");
     }
 }
