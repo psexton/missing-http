1b6ca1df60e4e799cde46c98242c4d5c782708ef
Paul Sexton <psexton@gnresound.com>
2015-02-06 20:50:50 +0000

    refactor(matlab): head uses MatlabShim
    Refs #1

--- matlab/+http/head.m
+++ matlab/+http/head.m
@@ -9,19 +9,10 @@ function statusCode = head( url, varargin )
 %   auth caches. Also useful for checking auth before making a large POST 
 %   or PUT request to a server that doesn't support 100 Continue.
 
-% Build the request
-request = net.psexton.ext.org.apache.http.client.methods.HttpHead(url);
-if(nargin > 1)
-    http.private.addExtraHeaders(request, varargin);
-end
-
-% Execute the request
-[client, response] = http.private.executeRequest(request);
-
-% Parse the response
-statusCode = response.getStatusLine.getStatusCode;
-
-% Clean up
-http.private.cleanup(client, response);
+% Java arguments will map from char arrays and cell arrays of chars to
+% String and String[]
+% The Java response is a String. We convert this to a char array
+response = char(net.psexton.missinghttp.MatlabShim.head(url, varargin));
+statusCode = str2double(response);
 
 end
